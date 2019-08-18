2a5920b32cc73e4ad0aed94b9194b8babb74ac19
Matt Blum <matt.blum@live.com>
2019-08-18 12:15:03 -0500

    Fix service worker fetch listener

--- sw.js
+++ sw.js
@@ -10,42 +10,43 @@ self.addEventListener('install', event => {
     caches.open(staticCacheName).then(cache => {
       return cache.addAll([
         '/',
-        'index.html',
+        '/index.html',
         '/restaurant.html',
-        'manifest.json',
+        '/manifest.json',
         'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
-        'css/styles.css',
-        'css/xs.css',
-        'css/sm.css',
-        'css/md.css',
-        'css/lg.css',
+        '/css/styles.css',
+        '/css/xs.css',
+        '/css/sm.css',
+        '/css/md.css',
+        '/css/lg.css',
         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-        'icons/icon-192.png',
-        'icons/icon-512.png',
-        'img/1-576.jpg',
-        'img/1-800.jpg',
-        'img/2-576.jpg',
-        'img/2-800.jpg',
-        'img/3-576.jpg',
-        'img/3-800.jpg',
-        'img/4-576.jpg',
-        'img/4-800.jpg',
-        'img/5-576.jpg',
-        'img/5-800.jpg',
-        'img/6-576.jpg',
-        'img/6-800.jpg',
-        'img/7-576.jpg',
-        'img/7-800.jpg',
-        'img/8-576.jpg',
-        'img/8-800.jpg',
-        'img/9-576.jpg',
-        'img/9-800.jpg',
-        'img/10-576.jpg',
-        'img/10-800.jpg',
-        'js/dbhelper.js',
-        'js/idb.js',
-        'js/main.js',
-        'js/restaurant_info.js',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png',
+        '/img/1-576.jpg',
+        '/img/1-800.jpg',
+        '/img/2-576.jpg',
+        '/img/2-800.jpg',
+        '/img/3-576.jpg',
+        '/img/3-800.jpg',
+        '/img/4-576.jpg',
+        '/img/4-800.jpg',
+        '/img/5-576.jpg',
+        '/img/5-800.jpg',
+        '/img/6-576.jpg',
+        '/img/6-800.jpg',
+        '/img/7-576.jpg',
+        '/img/7-800.jpg',
+        '/img/8-576.jpg',
+        '/img/8-800.jpg',
+        '/img/9-576.jpg',
+        '/img/9-800.jpg',
+        '/img/10-576.jpg',
+        '/img/10-800.jpg',
+        '/sw.js',
+        '/js/dbhelper.js',
+        '/js/idb.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
         'https://fonts.googleapis.com/css?family=Roboto&display=swap'
         // 'http://localhost:1337/restaurants' // Use IndexDB for cahcing JSON
       ]);
@@ -71,17 +72,39 @@ self.addEventListener('activate', event => {
 });
 
 // Use cached HTML pages if available
-self.addEventListener('fetch', event => {
-  //   console.log(event.request);
-  var requestUrl = new URL(event.request.url);
+// self.addEventListener('fetch', event => {
+//   //   console.log(event.request);
+//   var requestUrl = new URL(event.request.url);
 
-  if (requestUrl.origin === location.origin) {
-    if (requestUrl.pathname === '/') {
-      event.respondWith(caches.match('/'));
-      return;
-    } else if (requestUrl.pathname === '/restaurant.html') {
-      event.respondWith(caches.match('/restaurant.html'));
-      return;
-    }
-  }
+//   if (requestUrl.origin === location.origin) {
+//     if (requestUrl.pathname === '/') {
+//       event.respondWith(caches.match('/'));
+//       return;
+//     } else if (requestUrl.pathname === '/restaurant.html') {
+//       event.respondWith(caches.match('/restaurant.html'));
+//       return;
+//     }
+//   }
+// });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (event.request.url.indexOf(staticCacheName) > -1) {
+        return fetch(event.request);
+      }
+      if (response) {
+        console.log('Serve from cache', response);
+        return response;
+      }
+      return fetch(event.request).then(response =>
+        caches.open(CURRENT_CACHES.prefetch).then(cache => {
+          // Cache response after making a request
+          cache.put(event.request, response.clone());
+          // Return original response
+          return response;
+        })
+      );
+    })
+  );
 });
